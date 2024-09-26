92ddedd2c4896ca58a3cb56a4e2b87f547f466cb
LE566 <quiq28615@gmail.com>
2024-09-26 12:20:57 -0600

    V5 Static Map Mark

--- backend/staticMap.js
+++ backend/staticMap.js
@@ -1,11 +1,26 @@
+// Initialize and add the map
 let map;
 
 async function initMap() {
+  // The location of Uluru
+  const position = { lat: 21.814277468810232, lng: -102.76965344528594 };
+  // Request needed libraries.
+  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
+  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 17,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Universidad Tecnologica de Calvillo",
   });
 }
 
